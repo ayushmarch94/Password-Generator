8458383e3d82c60a8b24cccd9a9e7e6d4f56fd5f
AYUSH KUMAR <125579641+ayushmarch94@users.noreply.github.com>
2023-08-12 15:56:31 +0530

    updated password generation to more character sets

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,32 +1,102 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./Main.css";
 
 export default function Main() {
+  const [password, setPassword] = useState("");
+  const [length, setLength] = useState("");
+  const [includeCapital, setIncludeCapital] = useState(false);
+  const [includeSmall, setIncludeSmall] = useState(false);
+  const [includeNumber, setIncludeNumber] = useState(false);
+  const [includeSymbol, setIncludeSymbol] = useState(false);
+  const passwordRef = useRef(null);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-  }
+    if (!includeCapital && !includeSmall && !includeNumber && !includeSymbol) {
+      alert("Please select at least one checkbox!");
+      return;
+    }
+    let charset = "";
+    if (includeCapital) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (includeSmall) charset += "abcdefghijklmnopqrstuvwxyz";
+    if (includeNumber) charset += "0123456789";
+    if (includeSymbol) charset += "!@#$%^&*()";
+    let newPassword = "";
+    if (charset.length > 0) {
+      for (let i = 0; i < length; i++) {
+        newPassword += charset[Math.floor(Math.random() * charset.length)];
+      }
+    }
+    setPassword(newPassword);
+  };
+
+  const handleCopy = () => {
+    const passwordText = passwordRef.current.textContent;
+    navigator.clipboard.writeText(passwordText);
+    alert("Password copied");
+  };
+
   return (
     <div className="page">
       <div className="main">
         <div className="container">
           <div className="view">
-            <div className="output"><p> </p></div>
-            <div className="a"><p>Length</p></div>
-            <div className="a"><p>Capital</p></div>
-            <div className="a"><p>Small</p></div>
-            <div className="a"><p>Number</p></div>
-            <div className="a"><p>Symbol</p></div>
+            <div className="output" ref={passwordRef}>
+              <p>{password}</p>
+            </div>
+            <div className="alength">
+              <p>Length</p>
+            </div>
+            <div className="text">
+              <p>Capital</p>
+            </div>
+            <div className="text">
+              <p>Small</p>
+            </div>
+            <div className="text">
+              <p>Number</p>
+            </div>
+            <div className="text">
+              <p>Symbol</p>
+            </div>
           </div>
           <div className="formier">
-            <div className="copy"><img src={require('../Images/copy.png')} width={40} /></div>
+            <div className="copy" onClick={handleCopy}>
+              <img src={require("../Images/copy.png")} width={30} />
+            </div>
             <form onSubmit={handleSubmit}>
-              <input placeholder="5" className="length" type="number" />
-              <input className="capital" type="checkbox" />
-              <input className="small" type="checkbox" />
-              <input className="number" type="checkbox" />
-              <input className="symbol" type="checkbox" />
-              <button type="submit">Generate Password</button>
+              <input
+                className="length"
+                type="number"
+                value={length}
+                onChange={(e) => setLength(e.target.value)}
+                required
+              />
+              <input
+                className="capital"
+                type="checkbox"
+                checked={includeCapital}
+                onChange={(e) => setIncludeCapital(e.target.checked)}
+              />
+              <input
+                className="small"
+                type="checkbox"
+                checked={includeSmall}
+                onChange={(e) => setIncludeSmall(e.target.checked)}
+              />
+              <input
+                className="number"
+                type="checkbox"
+                checked={includeNumber}
+                onChange={(e) => setIncludeNumber(e.target.checked)}
+              />
+              <input
+                className="symbol"
+                type="checkbox"
+                checked={includeSymbol}
+                onChange={(e) => setIncludeSymbol(e.target.checked)}
+              />
+              <button className="btn" type="submit">Generate Password</button>
             </form>
           </div>
         </div>
