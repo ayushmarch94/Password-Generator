10503184b0665ce742645705e960c2a030ad08db
AYUSH KUMAR <125579641+ayushmarch94@users.noreply.github.com>
2023-10-02 11:39:49 +0530

    added range input

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -7,12 +7,23 @@ export default function Main() {
       <div className="main">
         <div className="container">
           <div className="show"></div>
-          <img src={require("../Images/eye.png")} width={30} />
-          <img src={require("../Images/copy.png")} width={30} />
+          <img src={require("../Images/eye.png")} alt="icon" width={30} />
+          <img src={require("../Images/copy.png")} alt="icon" width={30} />
         </div>
         <input id="length" type="range" />
-        <div id="checks">
-
+        <div id="bottompart">
+          <div id="checks">
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <input type="checkbox" />
+          </div>
+          <div id="char">
+            <p>A-Z</p>
+            <p>a-z</p>
+            <p>0-9</p>
+            <p>@#$%&</p>
+          </div>
         </div>
       </div>
     </div>
